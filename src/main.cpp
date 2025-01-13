a1c816336f28a413a52e14823ce4343e65dac852
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-13 09:55:40 +0100

    Wifi library added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-//Oliver Olsen s205443
-//Nils Wulff   s223968
+/**
+ * @file main.cpp
+ * @author Nils Wulff s223968 & Oliver Olsen s205443
+ * @brief 
+ * @version 0.1
+ * @date 2025-01-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 
+
+// 16b A webserver is a host that can send data at the request of a client.
+// 
 
 #include <Arduino.h>
 
